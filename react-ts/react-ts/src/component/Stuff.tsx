2564a5d6527e4d427e9a7a7379b6b6721ce3853d
Alex Merced <alex@alexmerced.dev>
2023-02-10 14:21:24 -0500

    update

---
+++ react-ts/react-ts/src/component/Stuff.tsx
@@ -0,0 +1,15 @@
+interface StuffProps {
+  word: string;
+  cheese?: "gouda"|"munster"|"brie";
+  something?: string | number;
+}
+
+export default function Stuff(props: StuffProps) {
+  return (
+    <div>
+      <h1>{props.word}</h1>
+      {props.cheese ? props.cheese : "no cheese"}
+      {props.something ? props.something : "no cheese"}
+    </div>
+  );
+}
