3381144f6918e701fd5dbc4a7a3309dbc9d33b74
Alex Merced <alex@alexmerced.dev>
2023-02-26 19:39:03 -0500

    update

--- css-loaders/server.js
+++ css-loaders/server.js
@@ -3,11 +3,21 @@ const express = require("express")
 const app = express()
 
 app.use(express.static("website"))
+app.use(express.json())
 
 app.get("/api", (req, res) => {
     setTimeout(() => {
         res.json({status: "complete"})
     }, 3000)
+})
+
+app.get("/fetch", (req, res) => {
+    res.json({"Response": "Hello"})
+})
+
+app.post("/fetch", (req, res) => {
+    console.log(req.body)
+    res.json({"Response": "Goodbye"})
 })
 
 app.get("/apy", (req, res) => {
